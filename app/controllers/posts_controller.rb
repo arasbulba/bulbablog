71c50db68fef8dcf6d46a76bed9cf860b7a3b83b
Aras <araz.gasimov@gmail.com>
2021-01-28 23:28:55 +0400

    first commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
 
 def index
+	@posts = Post.all.order("created_at DESC")
 end
 
 def new
@@ -8,7 +9,7 @@ def new
 end
 
 def create
-	@post = Post.new()
+	@post = Post.new(post_params)
 
 	if @post.save
 		redirect_to @post
